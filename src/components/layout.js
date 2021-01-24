e8b922138651f18078b5ddddee2512e8c9331008
BENJAMIN RAGSDALE <ben.ragsdale@gmail.com>
2021-01-24 15:29:25 -0600

    pushing asset and other chgs

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+//import { Link } from 'gatsby';
 import { Global, css } from '@emotion/core'
 import Helmet from 'react-helmet';
 import Header from './header/Header'
