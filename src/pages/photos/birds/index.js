cb8fe8307a141c4281825dfebfce21081f9ca0ad
BENJAMIN RAGSDALE <ben.ragsdale@gmail.com>
2021-06-15 18:46:40 -0500

    use static query to get info for photos page and gallery page

--- src/pages/photos/birds/index.js
+++ src/pages/photos/birds/index.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import Layout from '../../../components/layout'
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
-const birdsPhotos = () => {
-    return (
-        <Layout>
-            <h1>Hello from bird photos!</h1>
-        </Layout>
-    )
-
+const imgStyles = {
+    height: '70vh',
+    width: 'auto',
 }
 
-export default birdsPhotos;
+const sectionStyles = {
+    display: 'inline-block',
+    paddingRight: '40px',
+}
+
+const headerStyles = {
+    textAlign: 'center',
+    fontFamily: 'helvetica, Arial, sans-serif',
+    margin: '4px 0'
+}
+
+const BirdsPhotos = () => {
+    const data = useStaticQuery(graphql`
+           query BirdPhotos {
+               allImagesJson(filter: {galleries: {name: {eq: "birds"}}}) {
+                edges {
+                  node {
+                    id
+                    alt
+                    descr
+                    path
+                  }
+                }
+        }
+    }`);
+    const posts = data.allImagesJson.edges.map(img => 
+    <div style={sectionStyles}>
+        <img key={img.node.id} style={imgStyles} src={img.node.path} alt={img.node.alt} />
+        <h4 style={headerStyles}>{img.node.descr} </h4>
+    </div>
+    );
+    return (
+        <Layout>
+            <section>
+                {posts}
+            </section>
+        </Layout>
+    )
+}
+
+
+export default BirdsPhotos;
--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import Layout from '../../components/layout'
-
-const photosIndex = () => {
+import { useStaticQuery, graphql } from "gatsby";
+const capitalize = s => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
+const PhotosIndex = () => {
+const data = useStaticQuery(graphql`
+    query AllPhotos {
+        allImagesJson(filter: {galleries: {featured: {eq: true}}}) {
+            edges {
+                node {
+                    id
+                    alt
+                    descr
+                    path
+                    galleries {
+                        name
+                      }
+                }
+            }
+        }
+    }`);
+const imgGalleries = data.allImagesJson.edges.map(img => 
+    <div>
+        <a href={`/photos/${img.node.galleries.name}`}>
+            <img key={img.node.id} src={img.node.path} />
+            <h4>{capitalize(`${img.node.galleries.name}`)}</h4>
+        </a>
+    </div>
+    
+);
     return (
         <Layout>
-        <h1>Hello from photos!</h1>
+            {imgGalleries}
         </Layout>
     )
 }
-export default photosIndex; 
+export default PhotosIndex; 
